d3b7cfbadfe4acdf64662a3dc6327df510a92d65
lqk <lqk123456>
2021-11-11 11:15:03 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/ikun/springcloud/entities/Payment.java
@@ -0,0 +1,11 @@
+package com.ikun.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class Payment {
+    private int id;
+    private String serial;
+}
---
+++ cloud-provider-payment8001/src/main/java/com/ikun/springcloud/service/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ikun.springcloud.service;
+
+import com.ikun.springcloud.dao.PaymentDao;
+import com.ikun.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentDao paymentDao;
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
